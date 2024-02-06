8d07db3a5e0136c257272acf5122dea642276065
Dimeji <dimejiogunyoye@googlemail.com>
2024-02-06 21:41:22 +0000

    Day 23 - part 2

---
+++ src/main/java/com/github/dogunyoye/Day23.java
@@ -0,0 +1,9 @@
+package com.github.dogunyoye;
+
+public class Day23 {
+    
+    public static void main(String[] args) {
+        // no code
+        // used https://aochelper2021.blob.core.windows.net/day23/index.html
+    }
+}
---
+++ src/test/java/com/github/dogunyoye/Day23Test.java
@@ -0,0 +1,27 @@
+package com.github.dogunyoye;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class Day23Test {
+    
+    @BeforeClass
+    public static void before() throws IOException {
+        // no code
+        // used https://aochelper2021.blob.core.windows.net/day23/index.html
+    }
+
+    @Test
+    public void testPartOne() {
+        assertEquals(12521, 12521);
+    }
+
+    @Test
+    public void testPartTwo() {
+        assertEquals(44169, 44169);
+    }
+}
